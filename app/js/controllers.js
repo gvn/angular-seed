15eaf48bd768e9d27f7117e44bf27e517b5be658
Gvn Lazar Suntop <gavin@gsuntop.com>
2014-04-07 17:12:54 -0700

    all about the hashbangz

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,11 @@
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', [
-    function () {
-
+  .controller('MyCtrl1', ['$scope',
+    function ($scope) {
+      $scope.viewID = 1;
     }
   ])
-  .controller('MyCtrl2', [
-    function () {
-
+  .controller('MyCtrl2', ['$scope',
+    function ($scope) {
+      $scope.viewID = 2;
     }
   ]);
